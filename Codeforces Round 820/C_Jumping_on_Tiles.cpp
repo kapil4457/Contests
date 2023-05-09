5db6ca7f64eba6f50154aa4fe894f8ff9e6f4068
Kapil <kapilsoni54768161@gmail.com>
2023-05-09 22:20:16 +0530

    Upsolved Problem C of Contest no. 820

--- Codeforces Round 820/C_Jumping_on_Tiles.cpp
+++ Codeforces Round 820/C_Jumping_on_Tiles.cpp
@@ -1,14 +1,75 @@
 #include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define mod 1000000007ll //998244353ll
+#define mii map<int, int>
+#define pii pair<int, int>
+#define TEST int t;cin >>t;while(t--)
+int power(int a,int b){int result =1; while(b>0){if(b%2) result*=a; a*=a;b/=2;} return result;}
 using namespace std;
-#define ll as long long int
+void Solve(){
+
+string s;
+cin>>s;
+map<char,vector<int>>ref;
+for(int i=0;i<s.size();i++){
+    ref[s[i]].push_back(i+1);
+}
+int n = s.size();
+if(s[0] > s[n-1]){
+int jmp =0 ;
+int cst = abs(s[0]-s[n-1]);
+vector<int>path;
+for(int i =0 ; s[0]+i >= s[n-1] ; i--){
+    char c = s[0]+i;
+    if(ref.find(c)!=ref.end()){
+        for(auto v  :ref[c]){
+path.push_back(v);
+        }
+
+    }
+}
+
+cout<<cst<<" "<<path.size()<<endl;
+for(auto it : path){
+    cout<<it<<" ";
+}
+
+cout<<endl;
+}else if(s[0] < s[n-1]){
+    int jmp = 0;
+    int cst = abs(s[0]-s[n-1]);
+    vector<int>path;
+
+    for(int i =0 ; s[0]+i <= s[n-1] ; i ++){
+        char c = s[0]+i;
+        if(ref.find(c)!=ref.end()){
+            for(auto it : ref[c]){
+                path.push_back(it);
+            }
+
+        }
+    }
+
+    cout<<cst<<" "<<path.size()<<endl;
+ for(auto it : path){
+    cout<<it<<" ";
+}
+
+cout<<endl;   
+}else{
+    cout<<0<<" "<<ref[s[0]].size()<<endl;
+     for(auto it : ref[s[0]]){
+    cout<<it<<" ";
+}
+
+cout<<endl;   
+}
+}
 int main()
 {
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        string s;
-        cin >> s;
-    }
-    return 0;
+TEST {
+Solve();
+}
+ return 0;
 }
